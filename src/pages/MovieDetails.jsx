fd97b4baf3efd128e9c7075002deb1eea99aedd0
alicjapol <alicjaxpoltorak@gmail.com>
2024-02-17 08:21:32 +0100

    Added user score and genres to movie details page

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -27,6 +27,8 @@ export default function MovieDetails() {
           <h2>{movie.title}</h2>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
           <p>{movie.overview}</p>
+          <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
+          <p>Genres: {movie.genres.map(genre => genre.name).join(', ')}</p>
           <Link to="cast">Show Cast</Link>
           <Link to="reviews">Show Reviews</Link>
           <Outlet />
@@ -35,4 +37,3 @@ export default function MovieDetails() {
     </PageContainer>
   );
 }
-
